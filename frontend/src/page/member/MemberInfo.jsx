276b6e1bd6ed63ff357ed28da29c630b0de866a9
seokminlee24 <lsm67286564@gmail.com>
2025-01-02 11:39:47 +0900

    feat: 회원 정보 보기에서 성별(M,F) 나오는 것을 남자 또는 여자로 나오게 하기

--- frontend/src/page/member/MemberInfo.jsx
+++ frontend/src/page/member/MemberInfo.jsx
@@ -12,9 +12,13 @@ export function MemberInfo() {
     //회원정보 열기
     axios.get(`/api/member/${memberId}`).then((res) => setMember(res.data));
   }, []);
+
   if (!member) {
     return <Spinner />;
   }
+
+  const genderText = member.gender === "M" ? "남자" : "여자";
+
   return (
     <Box>
       <Heading>회원 정보</Heading>
@@ -29,7 +33,7 @@ export function MemberInfo() {
           <Input readOnly value={member.nickname} />
         </Field>
         <Field label={"성별"}>
-          <Input readOnly value={member.gender} />
+          <Input readOnly value={genderText} />
         </Field>
         <Field label={"암호"}>
           <Input type={"datetime-local"} readOnly value={member.inserted} />
