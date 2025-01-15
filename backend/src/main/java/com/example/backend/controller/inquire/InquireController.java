687501865c97d05afb2f3db3080cd72df9c8a519
seokminlee24 <lsm67286564@gmail.com>
2025-01-15 10:06:03 +0900

    feat: 관리자도 다른 문의글 수정 가능하게 및 자기 아이디는 자기만 가능하게

--- backend/src/main/java/com/example/backend/controller/inquire/InquireController.java
+++ backend/src/main/java/com/example/backend/controller/inquire/InquireController.java
@@ -18,20 +18,27 @@ public class InquireController {
 
     // 문의글 수정
     @PutMapping("/inquireUpdate")
-    public ResponseEntity<Map<String, Object>> inquireUpdate(@RequestBody Inquire inquire) {
-        if (service.validate(inquire)) {
-            if (service.inquireUpdate(inquire)) {
-                return ResponseEntity.ok()
-                        .body(Map.of("message", Map.of("type", "success"
-                                , "text", STR."\{inquire.getInquireId()}번 문의글이 수정되었습니다.")));
+    @PreAuthorize("isAuthenticated() or hasAuthority('SCOPE_admin')")
+    public ResponseEntity<Map<String, Object>> inquireUpdate(@RequestBody Inquire inquire, Authentication authentication) {
+        if (service.hasAccess(inquire.getInquireId(), authentication) || service.isAdmin(authentication)) {
+            if (service.validate(inquire)) {
+                if (service.inquireUpdate(inquire)) {
+                    return ResponseEntity.ok()
+                            .body(Map.of("message", Map.of("type", "success"
+                                    , "text", STR."\{inquire.getInquireId()}번 문의글이 수정되었습니다.")));
+                } else {
+                    return ResponseEntity.internalServerError()
+                            .body(Map.of("message", Map.of("type", "error"
+                                    , "text", "문의글 수정 중 문제가 발생하였습니다.")));
+                }
             } else {
-                return ResponseEntity.internalServerError()
-                        .body(Map.of("message", Map.of("type", "error"
-                                , "text", "문의글 수정 중 문제가 발생하였습니다.")));
+                return ResponseEntity.badRequest().body(Map.of("message", Map.of("type", "warning",
+                        "text", "제목이나 문의 내용이 비어있을 수 없습니다.")));
             }
         } else {
-            return ResponseEntity.badRequest().body(Map.of("message", Map.of("type", "warning",
-                    "text", "제목이나 문의 내용이 비어있을 수 없습니다.")));
+            return ResponseEntity.status(403)
+                    .body(Map.of("message", Map.of("type", "error"
+                            , "text", "수정 권한이 없습니다.")));
         }
     }
 
