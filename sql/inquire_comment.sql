00640695a83c65b2030d5c217dfdded198ab929c
seokminlee24 <lsm67286564@gmail.com>
2025-01-15 17:44:54 +0900

    feat: 문의글 코멘트.sql 등록하기전

--- sql/inquire_comment.sql
+++ sql/inquire_comment.sql
@@ -1,3 +1,10 @@
 USE prj1230;
 
-CREATE TABLE inquire_comment;
+CREATE TABLE inquire_comment
+(
+    inquire_comment_id INT PRIMARY KEY AUTO_INCREMENT,
+    inquire_id         INT          NOT NULL REFERENCES inquire (inquire_id),
+    member_id          VARCHAR(50)  NOT NULL REFERENCES member (member_id),
+    inquire_comment    VARCHAR(300) NOT NULL,
+    inserted           DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
