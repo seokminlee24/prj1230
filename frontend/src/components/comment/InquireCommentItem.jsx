32269fd323f488a1b5e62f73efba4cbefd370feb
seokminlee24 <lsm67286564@gmail.com>
2025-01-17 14:10:36 +0900

    feat: 문의글 댓글 리스트

--- frontend/src/components/comment/InquireCommentItem.jsx
+++ frontend/src/components/comment/InquireCommentItem.jsx
@@ -5,7 +5,7 @@ export function InquireCommentItem({ inquireComment }) {
     <Box boarder={"1px solid black"} m={5}>
       <Flex justify={"space-between"}>
         <h3>{inquireComment.memberId}</h3>
-        <h4>{inquireComment.inserted}</h4>
+        <h4>{inquireComment.inserted.replace("T", " ")}</h4>
       </Flex>
       <p>{inquireComment.inquireComment}</p>
     </Box>
