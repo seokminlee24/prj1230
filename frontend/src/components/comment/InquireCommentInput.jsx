15ad6ef8c7ae401114eee82e1b44992a8769d1a7
seokminlee24 <lsm67286564@gmail.com>
2025-01-17 17:44:36 +0900

    feat: 로그인 되어 있을 때만 댓글 작성 가능하게

--- frontend/src/components/comment/InquireCommentInput.jsx
+++ frontend/src/components/comment/InquireCommentInput.jsx
@@ -1,18 +1,23 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import { Box, Group, Textarea } from "@chakra-ui/react";
 import { Button } from "../ui/button.jsx";
+import { AuthenticationContext } from "../context/AuthenticationProvider.jsx";
 
 export function InquireCommentInput({ inquireId, onSaveClick }) {
   const [inquireComment, setInquireComment] = useState("");
+  const { isAuthenticated } = useContext(AuthenticationContext);
 
   return (
     <Box>
       <Group>
         <Textarea
           value={inquireComment}
+          disabled={!isAuthenticated}
+          placeholder={isAuthenticated ? "" : "로그인 후 댓글을 남겨주세요."}
           onChange={(e) => setInquireComment(e.target.value)}
         />
         <Button
+          disabled={!isAuthenticated}
           onClick={() => {
             setInquireComment("");
             onSaveClick(inquireComment);
