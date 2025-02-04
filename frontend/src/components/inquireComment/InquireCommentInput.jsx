4074012cec4c2d5aba029a755be2e2f4f3e07f11
seokminlee24 <lsm67286564@gmail.com>
2025-02-05 02:56:47 +0900

    댓글창 꾸미기

--- frontend/src/components/inquireComment/InquireCommentInput.jsx
+++ frontend/src/components/inquireComment/InquireCommentInput.jsx
@@ -1,5 +1,5 @@
 import { useContext, useState } from "react";
-import { Box, Group, Textarea } from "@chakra-ui/react";
+import { Box, Flex, Textarea } from "@chakra-ui/react";
 import { Button } from "../ui/button.jsx";
 import { AuthenticationContext } from "../context/AuthenticationProvider.jsx";
 
@@ -13,23 +13,29 @@ export function InquireCommentInput({ inquireId, onSaveClick }) {
 
   return (
     <Box>
-      <Group>
+      <Flex alignItems="stretch">
         <Textarea
+          flex="1"
+          resize="vertical" // 세로로 크기 조정 가능하게 설정
           value={inquireComment}
           disabled={!isAuthenticated}
           placeholder={isAuthenticated ? "" : "로그인 후 댓글을 남겨주세요."}
+          borderRadius="md 0 0 md"
           onChange={(e) => setInquireComment(e.target.value)}
         />
         <Button
+          flex="0 1 auto" // 버튼의 크기를 유동적으로 설정
+          h="auto" // 버튼 높이를 자동으로 맞춤
           disabled={!isAuthenticated}
           onClick={() => {
             setInquireComment("");
             onSaveClick(inquireComment);
           }}
+          borderRadius="0 md md 0"
         >
           문의 댓글 쓰기
         </Button>
-      </Group>
+      </Flex>
     </Box>
   );
 }
